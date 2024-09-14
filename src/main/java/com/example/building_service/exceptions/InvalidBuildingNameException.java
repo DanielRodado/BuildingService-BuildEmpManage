5e584bf032c23b37f43faf1f506ec993f99200b8
DanielRodado <d4nielrodado@gmail.com>
2024-09-14 18:35:28 -0500

    Excepción para cuando el nombre de un edificio sea invalido

---
+++ src/main/java/com/example/building_service/exceptions/InvalidBuildingNameException.java
@@ -0,0 +1,9 @@
+package com.example.building_service.exceptions;
+
+public class InvalidBuildingNameException extends RuntimeException {
+
+    public InvalidBuildingNameException(String message) {
+        super(message);
+    }
+
+}
