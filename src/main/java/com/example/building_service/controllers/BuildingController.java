c02e1fcd0333e8b572055cff30ceb98d9a8d9ec6
DanielRodado <d4nielrodado@gmail.com>
2024-09-14 21:54:34 -0500

    Controlador con métodos para obtener 'Buildings'

--- src/main/java/com/example/building_service/controllers/BuildingController.java
+++ src/main/java/com/example/building_service/controllers/BuildingController.java
@@ -1,5 +1,30 @@
 package com.example.building_service.controllers;
 
+import com.example.building_service.model.BuildingEntity;
+import com.example.building_service.service.BuildingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@RestController
+@RequestMapping("/api/buildings")
 public class BuildingController {
+
+    @Autowired
+    private BuildingService buildingService;
+
+    @GetMapping("/{buildingId}")
+    public Mono<BuildingEntity> getBuildingById(@PathVariable Long buildingId) {
+        return buildingService.getBuildingById(buildingId);
+    }
+
+    @GetMapping
+    public Flux<BuildingEntity> getAllBuildings() {
+        return buildingService.getAllBuildings();
+    }
+
 }
