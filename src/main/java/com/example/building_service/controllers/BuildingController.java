5a17a2176dc7fd204f9fc47e647c11bd4332c745
DanielRodado <d4nielrodado@gmail.com>
2024-09-15 20:10:14 -0500

    Método agregado al controlador

--- src/main/java/com/example/building_service/controllers/BuildingController.java
+++ src/main/java/com/example/building_service/controllers/BuildingController.java
@@ -3,10 +3,7 @@ package com.example.building_service.controllers;
 import com.example.building_service.model.BuildingEntity;
 import com.example.building_service.service.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,6 +22,12 @@ public class BuildingController {
     @GetMapping
     public Flux<BuildingEntity> getAllBuildings() {
         return buildingService.getAllBuildings();
+    }
+
+    @GetMapping("/capacity/between")
+    public Flux<BuildingEntity> getAllBuildingsIfCapacityBetweenIn(@RequestParam(defaultValue = "0") int minCapacity,
+                                                                   @RequestParam(defaultValue = "200") int maxCapacity) {
+        return buildingService.getAllBuildingsByCapacityBetween(minCapacity, maxCapacity);
     }
 
 }
