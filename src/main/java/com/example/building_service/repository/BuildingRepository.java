dfb7ee3201a3fd0aab2891b8becaaf1fc33579cb
DanielRodado <d4nielrodado@gmail.com>
2024-09-15 20:08:53 -0500

    Método en el repositorio para buscar entre x & y capacidad

--- src/main/java/com/example/building_service/repository/BuildingRepository.java
+++ src/main/java/com/example/building_service/repository/BuildingRepository.java
@@ -2,6 +2,10 @@ package com.example.building_service.repository;
 
 import com.example.building_service.model.BuildingEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface BuildingRepository extends ReactiveCrudRepository<BuildingEntity, Long> {
+
+    Flux<BuildingEntity> findByCapacityBetween(int minCapacity, int maxCapacity);
+
 }
