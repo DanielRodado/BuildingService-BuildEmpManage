0dff2834b8bbf946d2f71b2f9434076ee5c976ee
DanielRodado <d4nielrodado@gmail.com>
2024-09-26 14:29:01 -0500

    Se añadieron Utils de constantes de mensajes para cuerpos de respuestas

---
+++ src/main/java/com/example/building_service/utils/Messages.java
@@ -0,0 +1,6 @@
+package com.example.building_service.utils;
+
+public final class Messages {
+    public static String BUILDING_NOT_FOUND = "The building was not found.";
+    public static String BUILDING_EXISTS = "The building already exists.";
+}
